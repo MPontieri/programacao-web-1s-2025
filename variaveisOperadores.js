118058bb9558f24e9db26ae2f248237452bc148f
MPontieri <139174251+MPontieri@users.noreply.github.com>
2025-03-06 12:27:08 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,17 @@
+// Declaração de variáveis numéricas para cada operador
+let numA = 10;
+let numB = 5;
+
+// Operações
+let soma = numA + numB;
+let subtracao = numA - numB;
+let multiplicacao = numA * numB;
+let divisao = numA / numB;
+let modulo = numA % numB;
+
+// Impressão dos resultados
+console.log(`Resultado da soma entre ${numA} e ${numB} é ${soma}`);
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${divisao}`);
+console.log(`Resultado do módulo entre ${numA} e ${numB} é ${modulo}`);
